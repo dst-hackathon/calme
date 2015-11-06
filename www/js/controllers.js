ba14db7564c0ddbe65d35001fe3b5effb3fc2145
Kowit Laison <kowit.laison@gmail.com>
2015-11-06 11:28:13 +0700

    Add UI mockup

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('billCtrl', function($scope) {
+
+})
+   
+.controller('sharesCtrl', function($scope) {
+
+})
+   
+.controller('dishesCtrl', function($scope) {
+
+})
+   
+.controller('payCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,55 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('bill', {
+      url: '/bill',
+      templateUrl: 'templates/bill.html',
+      controller: 'billCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('shares', {
+      url: '/shares',
+      templateUrl: 'templates/shares.html',
+      controller: 'sharesCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('dishes', {
+      url: '/dishes',
+      templateUrl: 'templates/dishes.html',
+      controller: 'dishesCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('pay', {
+      url: '/pay',
+      templateUrl: 'templates/pay.html',
+      controller: 'payCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/bill');
+
+});
