4d7f9afe6b7497cb9d91b38b254cbe430d10a0ba
yukyankee <c21_ep@hotmail.com>
2015-11-06 17:40:55 +0700

    Add calculated grand total

--- www/js/controllers/billController.js
+++ www/js/controllers/billController.js
@@ -1,9 +1,16 @@
 angular.module('app.controllers')
 
 .controller('billCtrl', function($scope, BillService) {
- 	$scope.bill = BillService.getBill();
 
-  	$scope.addBill = function(bill) {
+  $scope.bill = BillService.getBill();
+
+  $scope.addBill = function(bill) {
 		BillService.setBill(angular.copy(bill));
 	};
+
+  $scope.calculateGrandTotal = function(bill) {
+  	BillService.setBill(angular.copy(bill));
+    $scope.bill = BillService.calculateGrandTotal();
+	};
+
 });
