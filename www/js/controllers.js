1742d195912689cd4d42c8028dd840f0ee3f1bce
yukyankee <c21_ep@hotmail.com>
2015-11-06 13:42:40 +0700

    create bill model for first page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,14 @@ angular.module('app.controllers', [])
 
 .controller('billCtrl', function($scope) {
 
+  $scope.addBill = function(bill) {
+		$scope.bill = angular.copy(bill);
+	};
 })
 
 .controller('sharesCtrl', function($scope) {
 	$scope.list = [];
-	
+
 	$scope.addPeople = function(user) {
 		$scope.list.push(angular.copy(user));
 		user.name = '';
@@ -27,6 +30,3 @@ angular.module('app.controllers', [])
 .controller('payCtrl', function($scope) {
 
 })
-
-
-
