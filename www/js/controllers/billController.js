8d0141ed9d7d6dd9ab97249f7ad326ede9a60950
oreoblizzard <prakrit.tan@gmail.com>
2015-11-06 17:05:58 +0700

    Modify bill service

--- www/js/controllers/billController.js
+++ www/js/controllers/billController.js
@@ -1,14 +1,9 @@
 angular.module('app.controllers')
 
 .controller('billCtrl', function($scope, BillService) {
- 	$scope.bill = {
- 		vat: 0,
- 		serviceCharge: 0
- 	};
+ 	$scope.bill = BillService.getBill();
 
   	$scope.addBill = function(bill) {
-  		$scope.bill = BillService.getBill();
-
 		BillService.setBill(angular.copy(bill));
 	};
 });
--- www/js/services/billService.js
+++ www/js/services/billService.js
@@ -1,16 +1,19 @@
 angular.module('app.services')
 
 .service('BillService', [function(){
-  var bill = {};
+  var bill = {
+    vat: 0,
+    serviceCharge: 0
+  };
 
   return {
 
     setBill: function(bill) {
-      this.bill = bill;
+      bill = bill;
     },
     
     getBill: function() {
-      return this.bill;
+      return bill;
     }
 
   };
