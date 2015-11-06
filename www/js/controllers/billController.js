b0d22760d9067bb7811c15648a16593cc857dcf1
oreoblizzard <prakrit.tan@gmail.com>
2015-11-06 16:54:32 +0700

    Initialize vat and service charge

--- www/js/controllers/billController.js
+++ www/js/controllers/billController.js
@@ -1,8 +1,12 @@
 angular.module('app.controllers')
 
 .controller('billCtrl', function($scope, BillService) {
+ 	$scope.bill = {
+ 		vat: 0,
+ 		serviceCharge: 0
+ 	};
 
-  $scope.addBill = function(bill) {
+  	$scope.addBill = function(bill) {
   		$scope.bill = BillService.getBill();
 
 		BillService.setBill(angular.copy(bill));
