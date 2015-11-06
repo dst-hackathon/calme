45a30ec0928b8cd4edfcd2a00f19281837e36327
smileyOpal <TRACWeb@DSTRSs-MacBook-Pro.local>
2015-11-06 17:30:26 +0700

    edit route for options/dishes/pay pages

--- www/js/services.js
+++ www/js/services.js
@@ -2,17 +2,4 @@ angular.module('app.services', [])
 
 .factory('BlankFactory', [function(){
 
-}])
-
-.service('OptionsService', [function(){
-
-  return {
-    option : 'E', //default to equal - 'E'qual, 'S'hare
-    set: function(option){
-      this.option = option;
-    },
-    get: function(){
-      return this.option;
-    }
-  };
 }]);
