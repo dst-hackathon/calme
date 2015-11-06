23a12d1fc7d775deb57a3019bb693cf52c9fae6e
Tharathorn Inphen <starkhorn@Tharathorns-MacBook-Air.local>
2015-11-06 15:00:27 +0700

    Add functionality to add a new dish

--- www/js/controllers/dishesController.js
+++ www/js/controllers/dishesController.js
@@ -2,11 +2,39 @@ angular.module('app.controllers')
 
 .controller('dishesCtrl', function($scope, DishesService) {
 
+  $scope.currentDish = DishesService.new();
+
+  $scope.dishes = DishesService.all();
   $scope.people = [
     { name: 'Ohm' },
     { name: 'Seph' }
   ];
 
-  $scope.dishes = DishesService.all();
+  $scope.isSelectedPerson = function(person) {
+    var currentPeople = $scope.currentDish.people;
+
+    return currentPeople.some(function(currentPerson) {
+      return person.name === currentPerson.name;
+    })
+  };
+
+  $scope.togglePerson = function(event, person) {
+    var checkbox = event.target,
+        currentPeople = $scope.currentDish.people;
+
+    if (checkbox.checked) {
+      currentPeople.push(person);
+    } else {
+      var position = currentPeople.indexOf(person);
+
+      currentPeople.splice(position, 1);
+    }
+  };
+
+  $scope.addDish = function() {
+    DishesService.add($scope.currentDish);
+
+    $scope.currentDish = DishesService.new();
+  };
 
 });
