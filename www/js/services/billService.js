ecdbd095c94b667fa9e06de6b19db6c2d5dab60d
Tisana Roongsawang <tisana@gmail.com>
2015-11-07 13:27:20 +0700

    Fix math calculation come into string

--- www/js/services/billService.js
+++ www/js/services/billService.js
@@ -21,10 +21,10 @@ angular.module('app.services')
 
     calculateGrandTotal: function(){
       bill.vatAmount = (bill.totalAmount * (bill.vat/100)).toFixed(2);
-      var afterVat = bill.totalAmount + bill.vatAmount;
+      var afterVat = Number(bill.totalAmount) + Number(bill.vatAmount);
 
       bill.serviceChargeAmount = (afterVat * (bill.serviceCharge/100)).toFixed(2);
-      var afterServiceCharge = afterVat + serviceChargeAmount;
+      var afterServiceCharge = Number(afterVat) + Number(bill.serviceChargeAmount);
 
       bill.grandTotal = afterServiceCharge;
       return bill;
