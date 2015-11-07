2bd57f53e5b33842ab17ff52ffc1148069170cb2
Tisana Roongsawang <tisana@gmail.com>
2015-11-07 13:34:41 +0700

    Fix a wrong merge

--- www/js/services/billService.js
+++ www/js/services/billService.js
@@ -21,13 +21,13 @@ angular.module('app.services')
 
     calculateGrandTotal: function(){
       if(bill.totalAmount) {
-      bill.vatAmount = (bill.totalAmount * (bill.vat/100)).toFixed(2);
-      var afterVat = Number(bill.totalAmount) + Number(bill.vatAmount);
+        bill.vatAmount = (bill.totalAmount * (bill.vat/100)).toFixed(2);
+        var afterVat = Number(bill.totalAmount) + Number(bill.vatAmount);
 
-      bill.serviceChargeAmount = (afterVat * (bill.serviceCharge/100)).toFixed(2);
-      var afterServiceCharge = Number(afterVat) + Number(bill.serviceChargeAmount);
-          
-      bill.grandTotal = afterServiceCharge;
+        bill.serviceChargeAmount = (afterVat * (bill.serviceCharge/100)).toFixed(2);
+        var afterServiceCharge = Number(afterVat) + Number(bill.serviceChargeAmount);
+
+        bill.grandTotal = afterServiceCharge;
       }
       return bill;
     }
